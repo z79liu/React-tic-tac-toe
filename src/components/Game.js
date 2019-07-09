7a104127b010dc701bfd4e0d7de40c8667d83dd8
Zeyu <haozai_03@hotmail.com>
2019-07-09 15:37:01 +0000

    added calculating winner

--- src/components/Game.js
+++ src/components/Game.js
@@ -15,7 +15,14 @@ export class Game extends Component {
   handleClick=(i)=>{
     const history = this.state.history.slice(0,this.state.turnCount+1);
     const current = history[history.length-1];
-    const squares = current.squares.slice()
+    const squares = current.squares.slice();
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({history: history.concat({
+      squares:squares
+    }),
+  xIsNext: !this.state.xIsNext,
+  turnCount: history.length
+})
   }
   render() {
     const history = this.state.history
@@ -31,4 +38,22 @@ export class Game extends Component {
   }
 }
 
+const calcWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+  for (let i=0; i<lines.length;i++){
+    const [a,b,c] = lines[i]
+    if(squares[a] && squares[a] === squares[b] && squares[b] === squares[c]){
+      return squares[a]
+    }
+  }
+}
 export default Game
